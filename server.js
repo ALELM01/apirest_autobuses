5d52764a097700505007b5574177a670c8b6f114
alejandroleonmunoz1007@gmail.com <alejandroleonmunoz1007@gmail.com>
2020-02-10 18:13:39 +0100

    modificando vistas

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
-
+var path = __dirname + '/views/';
+app.use("/src/public", express.static(__dirname + "/public"));
 //importing routes
+app.get("/", function (req, res) {
+    console.log("hola");
+    res.sendFile(path + "index.html");
+});
 
 
+app.get("/consorcio", function (req, res) {
+    console.log("hola");
+    res.sendFile(path + "consorcio.html");
+});
 
-
+app.get("/paradas", function (req, res) {
+    console.log("hola");
+    res.sendFile(path + "paradas.html");
+});
 
 
 
@@ -20,7 +32,6 @@ const app = express();
     
 //setings
 app.set('port', process.env.PORT || 3000);
-app.set('public', path.join(__dirname, 'public'));
 //starting the server
 app.listen(app.get('port'), () =>{
     console.log('server on port 3000');
